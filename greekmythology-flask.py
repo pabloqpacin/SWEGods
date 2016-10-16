5bbb42de301560dc07f604114a30bfff29183646
Susmita Hossain <shuuhossain@utexas.edu>
2016-10-16 17:16:11 -0500

    fixing typo in if-elif set, added 404 error codes

--- greekmythology-flask.py
+++ greekmythology-flask.py
@@ -29,28 +29,28 @@ def error_wrapper(content):
 def index():
 	if os.path.exists(app.config['STATIC_SPLASH_PAGE']):
 		return send_file(app.config['STATIC_SPLASH_PAGE'])
-	return error_wrapper('Hello, World! <SPLASH PAGE NOT YET ADDED>')
+	return error_wrapper('Hello, World! <SPLASH PAGE NOT YET ADDED>'), 404
 
 @app.route('/about')
 @app.route('/about/')
 def about_page():
 	if os.path.exists(app.config['STATIC_ABOUT_PAGE']):
 		return send_file(app.config['STATIC_ABOUT_PAGE'])
-	return error_wrapper('About page to be added')
+	return error_wrapper('About page to be added'), 404
 
 @app.route('/gods')
 @app.route('/gods/')
 def gods_model():
 	if os.path.exists(app.config['STATIC_GODS_LIST']):
 		return send_file(app.config['STATIC_GODS_LIST'])
-	return error_wrapper('Gods Model page to be added')
+	return error_wrapper('Gods Model page to be added'), 404
 	
 @app.route('/heros')
 @app.route('/heros/')
 def heros_model():
 	if os.path.exists(app.config['STATIC_HEROS_LIST']):
 		return send_file(app.config['STATIC_HEROS_LIST'])
-	return error_wrapper('Heros Model page to be added')
+	return error_wrapper('Heros Model page to be added'), 404
 
 #using string instead of path because we don't want '/' to count
 @app.route('/gods/<string:god>') 
@@ -59,7 +59,7 @@ def god_page(god):
 	if os.path.exists(os.path.join(app.config['STATIC_GODS_FOLDER'], god.lower() + ".html")):
 		return send_from_directory(app.config['STATIC_GODS_FOLDER'],
                                god.lower() + '.html', as_attachment=False)
-	return error_wrapper('Page for god: ' + god + ' to be added')
+	return error_wrapper('Page for god: ' + god + ' to be added'), 404
 	
 @app.route('/heros/<string:hero>')
 @app.route('/heros/<string:hero>/')
@@ -67,13 +67,13 @@ def hero_page(hero):
 	if os.path.exists(os.path.join(app.config['STATIC_HEROS_FOLDER'], hero.lower() + ".html")):
 		return send_from_directory(app.config['STATIC_HEROS_FOLDER'],
                                hero.lower() + '.html', as_attachment=False)
-	return error_wrapper('Page for hero: ' + hero + ' to be added')
+	return error_wrapper('Page for hero: ' + hero + ' to be added'), 404
 
 @app.route('/static/<string:folder>/<string:file>')
 def static_files(folder, file):
 	""" Serves the static files that will be used through all iterations of the project
 	"""
-	elif folder == 'css':
+	if folder == 'css':
 		return send_from_directory(app.config['STATIC_CSS_FOLDER'],
                                file.lower(), as_attachment=False)
 	elif folder == 'fonts':
@@ -85,3 +85,5 @@ def static_files(folder, file):
 	elif folder == 'images':
 		return send_from_directory(app.config['STATIC_IMAGES_FOLDER'],
                                file.lower(), as_attachment=False)
+	else:			
+		return error_wrapper('Invalid file'), 404
