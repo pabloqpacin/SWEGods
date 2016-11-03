017599ab9906ef0bcdf1f4800519c7c2c329786f
Ben Lin <ben.lin9@gmail.com>
2016-11-03 00:56:57 -0500

    updated models.py to use postgres URI

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/greekmythology'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5421/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
