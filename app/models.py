ec1323cd2624e86626cb84b230ad3201d9840534
Ben Lin <ben.lin9@gmail.com>
2016-11-14 23:58:22 -0600

    updated Location for correct columns

--- app/models.py
+++ app/models.py
@@ -96,12 +96,12 @@ class Hero(db.Model):
 
     name = db.Column(db.String, primary_key=True)
     hero_type = db.Column(db.String)
-    power = db.Column(db.String)
-    home = db.Column(db.String)
     father_god = db.Column(db.String, db.ForeignKey('god.name'))
     father_hero = db.Column(db.String, db.ForeignKey('hero.name'))
     mother_god = db.Column(db.String, db.ForeignKey('god.name'))
-    mother_hero = db.Column(db.String, db.ForeignKey('hero.name'))
+    mother_hero = db.Column(db.String, db.ForeignKey('hero.name'))  
+    power = db.Column(db.String)
+    home = db.Column(db.String)
 
     def __init__(self, name, hero_type, power, home):
         self.name = name
@@ -121,20 +121,18 @@ class Location(db.Model):
     __tablename__ = 'location'
 
     name = db.Column(db.String, primary_key=True)
-    creature_type = db.Column(db.String)
-    characteristics = db.Column(db.String)
-    related_characters = db.relationship('Character',
-                                         secondary=creatures_to_characters)
-    related_myths = db.relationship('Myth',
-                                    secondary=creatures_to_myths)
+    alt_name = db.Column(db.String)
+    myth = db.Column(db.String, db.ForeignKey('myth.name'))
+    location_type = db.Column(db.String)
+    god = db.Column(db.String, db.ForeignKey('god.name'))
 
-    def __init__(self, name, creature_type, characteristics):
-        super().__init__(self, name)
-        self.creature_type = creature_type
-        self.characteristics = characteristics
+    def __init__(self, name, alt_name, location_type):
+        self.name = name
+        self.alt_name = alt_name
+        self.location_type = location_type
 
     def __repr__(self):
-        return '<Creature %r>' % self.name
+        return '<Location %r>' % self.name
 
 class Myth(db.Model):
     """
