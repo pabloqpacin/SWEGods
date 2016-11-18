74f1cf957dfa4120855931bd672a1bc0b5c135ed
Susmita Hossain <shuuhossain@utexas.edu>
2016-11-17 20:53:33 -0600

    fixing tabbing vs spacing (we use 4 spaces)

--- IDB3.py
+++ IDB3.py
@@ -256,16 +256,16 @@ def error_wrapper(content):
     """
         Shows error message.
     """
-	return render_template('error_template.html', error_message=content)
+    return render_template('error_template.html', error_message=content)
 
 @app.route('/')
 def index():
     """
         Shows the splash page.
     """
-	if os.path.exists(app.config['STATIC_SPLASH_PAGE']):
-		return send_file(app.config['STATIC_SPLASH_PAGE'])
-	return error_wrapper('Hello, World! <SPLASH PAGE NOT YET ADDED>'), 404
+    if os.path.exists(app.config['STATIC_SPLASH_PAGE']):
+        return send_file(app.config['STATIC_SPLASH_PAGE'])
+    return error_wrapper('Hello, World! <SPLASH PAGE NOT YET ADDED>'), 404
 
 @app.route('/about')
 @app.route('/about/')
@@ -273,9 +273,9 @@ def about_page():
     """
         Connects to the about page.
     """
-	if os.path.exists(app.config['STATIC_ABOUT_PAGE']):
-		return send_file(app.config['STATIC_ABOUT_PAGE'])
-	return error_wrapper('About page to be added'), 404
+    if os.path.exists(app.config['STATIC_ABOUT_PAGE']):
+        return send_file(app.config['STATIC_ABOUT_PAGE'])
+    return error_wrapper('About page to be added'), 404
 
 @app.route('/gods')
 @app.route('/gods/')
@@ -283,9 +283,9 @@ def gods_model():
     """
         Connects to the gods page.
     """
-	if os.path.exists(app.config['STATIC_GODS_LIST']):
-		return send_file(app.config['STATIC_GODS_LIST'])
-	return error_wrapper('Gods Model page to be added'), 404
+    if os.path.exists(app.config['STATIC_GODS_LIST']):
+        return send_file(app.config['STATIC_GODS_LIST'])
+    return error_wrapper('Gods Model page to be added'), 404
 
 @app.route('/heroes')
 @app.route('/heroes/')
@@ -293,9 +293,9 @@ def heroes_model():
     """
         Connects to the heroes page.
     """
-	if os.path.exists(app.config['STATIC_HEROES_LIST']):
-		return send_file(app.config['STATIC_HEROES_LIST'])
-	return error_wrapper('Heroes Model page to be added'), 404
+    if os.path.exists(app.config['STATIC_HEROES_LIST']):
+        return send_file(app.config['STATIC_HEROES_LIST'])
+    return error_wrapper('Heroes Model page to be added'), 404
 
 @app.route('/locations')
 @app.route('/locations/')
@@ -303,9 +303,9 @@ def creatures_model():
     """
         Connects to creatures page
     """
-	if os.path.exists(app.config['STATIC_LOCATIONS_LIST']):
-		return send_file(app.config['STATIC_LOCATIONS_LIST'])
-	return error_wrapper('Locations Model page to be added'), 404
+    if os.path.exists(app.config['STATIC_LOCATIONS_LIST']):
+        return send_file(app.config['STATIC_LOCATIONS_LIST'])
+    return error_wrapper('Locations Model page to be added'), 404
 
 @app.route('/myths')
 @app.route('/myths/')
@@ -313,9 +313,9 @@ def myths_model():
     """
         Connects to myths page
     """
-	if os.path.exists(app.config['STATIC_MYTHS_LIST']):
-		return send_file(app.config['STATIC_MYTHS_LIST'])
-	return error_wrapper('Myths Model page to be added'), 404
+    if os.path.exists(app.config['STATIC_MYTHS_LIST']):
+        return send_file(app.config['STATIC_MYTHS_LIST'])
+    return error_wrapper('Myths Model page to be added'), 404
 
 @app.route('/performtests')
 def performtests():
@@ -455,130 +455,130 @@ def search_model():
 # @app.route('/gods/<string:god>')
 # @app.route('/gods/<string:god>/')
 # def god_page(god):
-# 	if os.path.exists(os.path.join(app.config['STATIC_GODS_FOLDER'], god.lower() + ".html")):
-# 		return send_from_directory(app.config['STATIC_GODS_FOLDER'],
+#     if os.path.exists(os.path.join(app.config['STATIC_GODS_FOLDER'], god.lower() + ".html")):
+#         return send_from_directory(app.config['STATIC_GODS_FOLDER'],
 #                                god.lower() + '.html', as_attachment=False)
-# 	return error_wrapper('Page for god: ' + god + ' to be added'), 404
+#     return error_wrapper('Page for god: ' + god + ' to be added'), 404
 
 @app.route('/gods/<string:god>')
 @app.route('/gods/<string:god>/')
 def god_page(god):
-	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-	json_url = os.path.join(SITE_ROOT, 'static/js', 'godsinfo.json')
-	data = json.load(open(json_url))
-	god_info = []
-	for i in data:
-		if i['name'].lower() == god:
-			god_info.append(i['name'])
-			god_info.append(i['romanname'])
-			god_info.append(i['symbol'])
-			god_info.append(i['power'])
-			god_info.append(i['father'])
-			god_info.append(i['mother'])
-			god_info.append(i['url'])
-	return render_template('godtemp.html', god = god_info)
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, 'static/js', 'godsinfo.json')
+    data = json.load(open(json_url))
+    god_info = []
+    for i in data:
+        if i['name'].lower() == god:
+            god_info.append(i['name'])
+            god_info.append(i['romanname'])
+            god_info.append(i['symbol'])
+            god_info.append(i['power'])
+            god_info.append(i['father'])
+            god_info.append(i['mother'])
+            god_info.append(i['url'])
+    return render_template('godtemp.html', god = god_info)
 
 # Links to specific hero given by hero name
 # @app.route('/heroes/<string:hero>')
 # @app.route('/heroes/<string:hero>/')
 # def hero_page(hero):
-# 	if os.path.exists(os.path.join(app.config['STATIC_HEROES_FOLDER'], hero.lower() + ".html")):
-# 		return send_from_directory(app.config['STATIC_HEROES_FOLDER'],
+#     if os.path.exists(os.path.join(app.config['STATIC_HEROES_FOLDER'], hero.lower() + ".html")):
+#         return send_from_directory(app.config['STATIC_HEROES_FOLDER'],
 #                                hero.lower() + '.html', as_attachment=False)
-# 	return error_wrapper('Page for hero: ' + hero + ' to be added'), 404
+#     return error_wrapper('Page for hero: ' + hero + ' to be added'), 404
 
 @app.route('/heroes/<string:hero>')
 @app.route('/heroes/<string:hero>/')
 def hero_page(hero):
-	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-	json_url = os.path.join(SITE_ROOT, 'static/js', 'heroesinfo.json')
-	data = json.load(open(json_url))
-	hero_info = []
-	for i in data:
-		if i['name'].lower() == hero:
-			hero_info.append(i['name'])
-			hero_info.append(i['herotype'])
-			hero_info.append(i['power'])
-			hero_info.append(i['home'])
-			hero_info.append(i['father'])
-			hero_info.append(i['mother'])
-			hero_info.append(i['url'])
-	return render_template('herotemp.html', hero = hero_info)
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, 'static/js', 'heroesinfo.json')
+    data = json.load(open(json_url))
+    hero_info = []
+    for i in data:
+        if i['name'].lower() == hero:
+            hero_info.append(i['name'])
+            hero_info.append(i['herotype'])
+            hero_info.append(i['power'])
+            hero_info.append(i['home'])
+            hero_info.append(i['father'])
+            hero_info.append(i['mother'])
+            hero_info.append(i['url'])
+    return render_template('herotemp.html', hero = hero_info)
 
 # Links to specific creature given by creature name
 # @app.route('/locations/<string:location>')
 # @app.route('/locations/<string:location>/')
 # def creature_page(location):
-# 	if os.path.exists(os.path.join(app.config['STATIC_LOCATIONS_FOLDER'], location.lower() + ".html")):
-# 		return send_from_directory(app.config['STATIC_LOCATIONS_FOLDER'],
+#     if os.path.exists(os.path.join(app.config['STATIC_LOCATIONS_FOLDER'], location.lower() + ".html")):
+#         return send_from_directory(app.config['STATIC_LOCATIONS_FOLDER'],
 #                                location.lower() + '.html', as_attachment=False)
-# 	return error_wrapper('Page for creature: ' + location + ' to be added'), 404
+#     return error_wrapper('Page for creature: ' + location + ' to be added'), 404
 
 @app.route('/locations/<string:location>')
 @app.route('/locations/<string:location>/')
 def location_page(location):
-	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-	json_url = os.path.join(SITE_ROOT, 'static/js', 'locationsinfo.json')
-	data = json.load(open(json_url))
-	location_info = []
-	for i in data:
-		if i['name'].lower() == location:
-			location_info.append(i['name'])
-			location_info.append(i['altname'])
-			location_info.append(i['locationtype'])
-			location_info.append(i['myth'])
-			location_info.append(i['gods'])
-			location_info.append(i['url'])
-	return render_template('locationtemp.html', location = location_info)
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, 'static/js', 'locationsinfo.json')
+    data = json.load(open(json_url))
+    location_info = []
+    for i in data:
+        if i['name'].lower() == location:
+            location_info.append(i['name'])
+            location_info.append(i['altname'])
+            location_info.append(i['locationtype'])
+            location_info.append(i['myth'])
+            location_info.append(i['gods'])
+            location_info.append(i['url'])
+    return render_template('locationtemp.html', location = location_info)
 
 # Links to specific myth given by myth name
 # @app.route('/myths/<string:myth>')
 # @app.route('/myths/<string:myth>/')
 # def myth_page(myth):
-# 	if os.path.exists(os.path.join(app.config['STATIC_MYTHS_FOLDER'], myth.lower() + ".html")):
-# 		return send_from_directory(app.config['STATIC_MYTHS_FOLDER'],
+#     if os.path.exists(os.path.join(app.config['STATIC_MYTHS_FOLDER'], myth.lower() + ".html")):
+#         return send_from_directory(app.config['STATIC_MYTHS_FOLDER'],
 #                                myth.lower() + '.html', as_attachment=False)
-# 	return error_wrapper('Page for myth: ' + myth + ' to be added'), 404
+#     return error_wrapper('Page for myth: ' + myth + ' to be added'), 404
 
 @app.route('/myths/<string:myth>')
 @app.route('/myths/<string:myth>/')
 def myth_page(myth):
-	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-	json_url = os.path.join(SITE_ROOT, 'static/js', 'mythsinfo.json')
-	data = json.load(open(json_url))
-	myth_info = []
-	for i in data:
-		if i['name'].lower() == myth:
-			myth_info.append(i['name'])
-			myth_info.append(i['description'])
-			myth_info.append(i['theme'])
-			myth_info.append(i['place'])
-			myth_info.append(i['gods'])
-			myth_info.append(i['nongods'])
-			myth_info.append(i['url'])
-	return render_template('mythtemp.html', myth = myth_info)
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, 'static/js', 'mythsinfo.json')
+    data = json.load(open(json_url))
+    myth_info = []
+    for i in data:
+        if i['name'].lower() == myth:
+            myth_info.append(i['name'])
+            myth_info.append(i['description'])
+            myth_info.append(i['theme'])
+            myth_info.append(i['place'])
+            myth_info.append(i['gods'])
+            myth_info.append(i['nongods'])
+            myth_info.append(i['url'])
+    return render_template('mythtemp.html', myth = myth_info)
 
 @app.route('/static/<path:folder>/<string:file>')
 def static_files(folder, file):
-	""" Serves the static files that will be used through all iterations of the project
-	"""
-	if folder == 'css':
-		return send_from_directory(app.config['STATIC_CSS_FOLDER'],
+    """ Serves the static files that will be used through all iterations of the project
+    """
+    if folder == 'css':
+        return send_from_directory(app.config['STATIC_CSS_FOLDER'],
                                file.lower(), as_attachment=False)
-	elif folder == 'fonts':
-		return send_from_directory(app.config['STATIC_FONTS_FOLDER'],
+    elif folder == 'fonts':
+        return send_from_directory(app.config['STATIC_FONTS_FOLDER'],
                                file.lower(), as_attachment=False)
-	elif folder == 'js':
-		return send_from_directory(app.config['STATIC_JS_FOLDER'],
+    elif folder == 'js':
+        return send_from_directory(app.config['STATIC_JS_FOLDER'],
                                file.lower(), as_attachment=False)
-	elif folder == 'img':
-		return send_from_directory(app.config['STATIC_IMAGES_FOLDER'],
+    elif folder == 'img':
+        return send_from_directory(app.config['STATIC_IMAGES_FOLDER'],
                                file.lower(), as_attachment=False)
-	elif folder == 'img/about':
-		return send_from_directory(os.path.join(app.config['STATIC_IMAGES_FOLDER'], 'about'),
+    elif folder == 'img/about':
+        return send_from_directory(os.path.join(app.config['STATIC_IMAGES_FOLDER'], 'about'),
                                file.lower(), as_attachment=False)
-	else:
-		abort(code = 404)
+    else:
+        abort(code = 404)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
